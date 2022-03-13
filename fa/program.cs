12817dd1dab77f7c4b9d705607ddc45af4cac2e5
Alina Overina <alinaoverina@gmail.com>
2022-03-13 11:21:50 +0300

    first attempt

--- fa/program.cs
+++ fa/program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace fans
 {
   public class State
@@ -14,27 +15,157 @@ namespace fans
   }
 
 
-  public class FA1
+// Разработать класс ДКА, который допускает бинарную строку, 
+// содержащую ровно один '0' и хотя бы одну '1'. 
+  public class FA1 
   {
+    public static State a = new State()
+    {
+        Name = "a",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+    public State b = new State()
+    {
+        Name = "b",
+        IsAcceptState = true,
+        Transitions = new Dictionary<char, State>()
+    };
+    public State c = new State()
+    {
+        Name = "c",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+
+    State InitialState = a;
+    
+    public FA()
+    {
+        a.Transitions['0'] = b;
+        a.Transitions['1'] = a;
+        b.Transitions['0'] = c;
+        b.Transitions['1'] = b;
+        c.Transitions['0'] = c;
+        c.Transitions['1'] = c;            
+    }
+    
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+        State current = InitialState;
+        foreach (var c in s) // цикл по всем символам 
+        {
+            current = current.Transitions[c]; // меняем состояние на то, в которое у нас переход
+            if (current == null)              // если его нет, возвращаем признак ошибки
+                return null;
+            // иначе переходим к следующему
+        }
+        return current.IsAcceptState;         // результат true если в конце финальное состояние 
     }
   }
 
+
+  // Разработать класс ДКА, который допускает бинарную строку, 
+  // содержащую нечетное количество символов '0' и нечетное количество символов '1'.
   public class FA2
   {
+    public static State a = new State()
+    {
+        Name = "a",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+    public State b = new State()
+    {
+        Name = "b",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+    public State c = new State()
+    {
+        Name = "c",
+        IsAcceptState = true,
+        Transitions = new Dictionary<char, State>()
+    };
+    public State d = new State()
+    {
+        Name = "d",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+
+    State InitialState = a;
+    
+    public FA()
+    {
+        a.Transitions['0'] = d;
+        a.Transitions['1'] = b;
+        b.Transitions['0'] = c;
+        b.Transitions['1'] = a;
+        c.Transitions['0'] = b;
+        c.Transitions['1'] = d; 
+        d.Transitions['0'] = a;
+        d.Transitions['1'] = c;           
+    }
+    
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
-    }
+        State current = InitialState;
+        foreach (var c in s)
+        {
+            current = current.Transitions[c];
+            if (current == null)
+                return null;
+        }
+        return current.IsAcceptState;
+    } 
   }
   
+  // Разработать класс ДКА, который допускает бинарную строку, содержащую  '11'.
   public class FA3
   {
+    public static State a = new State()
+    {
+        Name = "a",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+    public State b = new State()
+    {
+        Name = "b",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+    public State c = new State()
+    {
+        Name = "c",
+        IsAcceptState = true,
+        Transitions = new Dictionary<char, State>()
+    };
+
+    State InitialState = a;
+    
+    public FA()
+    {
+        a.Transitions['0'] = a;
+        a.Transitions['1'] = b;
+        b.Transitions['0'] = а;
+        b.Transitions['1'] = b;
+        c.Transitions['0'] = c;
+        c.Transitions['1'] = c;            
+    }
+    
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+        State current = InitialState;
+        foreach (var c in s) // цикл по всем символам 
+        {
+            current = current.Transitions[c]; // меняем состояние на то, в которое у нас переход
+            if (current == null)              // если его нет, возвращаем признак ошибки
+                return null;
+            // иначе переходим к следующему
+        }
+        return current.IsAcceptState;         // результат true если в конце финальное состояние 
     }
   }
 
